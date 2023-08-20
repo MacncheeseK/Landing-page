273ce5b261696b7e70120dd09b0470078cdc80ee
DM <danymartinezluna@gmail.com>
2023-08-20 18:43:29 -0400

    Completed the footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,6 @@
+import { copyrightSign } from '../assets/icons';
 import { footerLogo } from '../assets/images';
-import { socialMedia } from '../constants';
+import { footerLinks, socialMedia } from '../constants';
 
 const Footer = () => {
   return (
@@ -15,12 +16,44 @@ const Footer = () => {
           </p>
           <div className="flex items-center  gap-5 mt-5">
             {socialMedia.map((icon) => (
-              <div>
+              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
                 <img src={icon.src} alt={icon.alt} width={24} height={24} />
               </div>
             ))}
           </div>
         </div>
+        <div className="flex flex-1 justify-between lg:gap-10 gap-20 flew-wrap">
+          {footerLinks.map((section) => (
+            <div key={section}>
+              <h4 className="text-white font-montserrat text-2xl mb-6 font-medium">
+                {section.title}
+              </h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li
+                    className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray cursor-pointer"
+                    key={link.name}
+                  >
+                    <a>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between text-white-400 mt-24 max-sm:items-center max-sm:flex-col">
+        <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+          <img
+            src={copyrightSign}
+            alt="copy right sign"
+            width={20}
+            height={20}
+            className="rounded-full m-0"
+          />
+          <p>Copyright. All rights reserved.</p>
+        </div>
+        <p className='font-montserrat cursor-pointer'>Terms & Conditions </p>
       </div>
     </footer>
   );

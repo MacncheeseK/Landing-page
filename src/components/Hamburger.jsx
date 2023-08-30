e22fa24caee9ba1e2c8bce6537d4d9542781904a
DM <danymartinezluna@gmail.com>
2023-08-29 20:41:44 -0400

    trying to add a drip down menu

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,6 +1,14 @@
-const Hamburger = () => {
+const Hamburger = (props) => {
   return (
-    <div>Hamburger</div>
-  )
+    
+      <li key={props.label}>
+        <a
+          href={props.href}
+          className=" font-montserrat leading-normal text-lg text-slate-gray"
+        >
+          {props.label}
+        </a>
+      </li>
+  );
 }
 export default Hamburger
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,25 @@
 import { headerLogo } from '../assets/images';
 import { hamburger } from '../assets/icons';
 import { navLinks } from '../constants';
-
+import { useState } from 'react';
+import Hamburger from './Hamburger';
 const Nav = () => {
-
-  
+  const [hamburgerMenu, setHamburgerMenu] = useState(false)
+  const toggleHamburgerMenu = () => {
+    setHamburgerMenu((prev)=> !prev); 
+  }
 
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
-          <img src={headerLogo} alt="Logo" width={130} height={29} className='m-0' />
+          <img
+            src={headerLogo}
+            alt="Logo"
+            width={130}
+            height={29}
+            className="m-0"
+          />
         </a>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
@@ -19,13 +28,22 @@ const Nav = () => {
                 href={item.href}
                 className=" font-montserrat leading-normal text-lg text-slate-gray"
               >
-                {item.label} 
+                {item.label}
               </a>
             </li>
           ))}
         </ul>
-        <div className='hidden max-lg:block'>
-            <img src={hamburger} alt='hamburgerMenu' width={25} height={25} onClick={hamburger}/>
+        <div className="hidden max-lg:block relative">
+          <button onClick={toggleHamburgerMenu}>
+            <img src={hamburger} alt="hamburgerMenu" width={25} height={25} />
+          </button>
+          
+          <ul className=" flex items-center gap-5 flex-col absolute right-0 bg-white p-3 shadow-lg rounded-md">
+            {hamburgerMenu &&
+              navLinks.map((item) => (
+                <Hamburger href={item.href} label={item.label} />
+              ))}
+          </ul>
         </div>
       </nav>
     </header>
